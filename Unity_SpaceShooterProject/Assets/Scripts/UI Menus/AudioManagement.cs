378cd8ef9d6526f46b614a008aa752d89abbfac9
MrTrainee <33583323+MrTrainee@users.noreply.github.com>
2018-01-22 18:58:00 +0200

    no one knows, it's a mystery
    audio functionality (inception)?!?!?

---
+++ Unity_SpaceShooterProject/Assets/Scripts/UI Menus/AudioManagement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManagement : MonoBehaviour
+{
+    private static AudioManagement instance = null;
+
+    public static AudioManagement Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+
+        DontDestroyOnLoad(instance.gameObject);
+    }
+}
