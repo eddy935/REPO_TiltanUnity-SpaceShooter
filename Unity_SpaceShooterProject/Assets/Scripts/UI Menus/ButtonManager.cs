3d49ace370ed56bdad4be91ff4eb84715b36bd19
MrTrainee <33583323+MrTrainee@users.noreply.github.com>
2018-01-29 17:49:59 +0200

    salvaged buttonmanager
    no connections yet

--- Unity_SpaceShooterProject/Assets/Scripts/UI Menus/ButtonManager.cs
+++ Unity_SpaceShooterProject/Assets/Scripts/UI Menus/ButtonManager.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ButtonManager : MonoBehaviour
 {
+    public Text musicText;
+    public Text SFXText;
+    public Text controlsText;
+
     public void SceneChangeButton (string scene)
     {
         SceneManager.LoadScene(scene);
@@ -13,5 +18,41 @@ public class ButtonManager : MonoBehaviour
     public void ExitGameButton ()
     {
         Application.Quit();
+    }
+
+    public void OnOffMusic()
+    {
+        if (musicText.text == "On")
+        {
+            musicText.text = "Off";
+        }
+        else
+        {
+            musicText.text = "On";
+        }
+    }
+
+    public void OnOffSFX()
+    {
+        if (SFXText.text == "On")
+        {
+            SFXText.text = "Off";
+        }
+        else
+        {
+            SFXText.text = "On";
+        }
+    }
+
+    public void ControlsTextTouchGyro()
+    {
+        if (controlsText.text == "Touch")
+        {
+            controlsText.text = "Gyro";
+        }
+        else
+        {
+            controlsText.text = "Touch";
+        }
     }
 }
